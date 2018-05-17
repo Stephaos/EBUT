3940d3864d59b58021482bce7eec57ffcb33176e
Markus Jäckle <mail@markus-jaeckle.de>
2018-05-17 10:07:33 +0200

    Use symlinks please...

--- Assignment3/WholesalerWebDemo/install/createdb.sh
+++ Assignment3/WholesalerWebDemo/install/createdb.sh
@@ -1,3 +1,3 @@
-/usr/local/mysql-5.7.22-macos10.13-x86_64/bin/mysqladmin --host=localhost --user=$2 --password=$3 drop $1 -f
-/usr/local/mysql-5.7.22-macos10.13-x86_64/bin/mysqladmin --host=localhost --user=$2 --password=$3 create $1 -f
-/usr/local/mysql-5.7.22-macos10.13-x86_64/bin/mysql --host=localhost --user=$2 --password=$3 --database=$1 < ebus.sql
+/usr/local/mysql/bin/mysqladmin --host=localhost --user=$2 --password=$3 drop $1 -f
+/usr/local/mysql/bin/mysqladmin --host=localhost --user=$2 --password=$3 create $1 -f
+/usr/local/mysql/bin/mysql --host=localhost --user=$2 --password=$3 --database=$1 < ebus.sql
