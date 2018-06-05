bfc452246625a8c292939e8f8c678b5363ac11fe
Markus Jäckle <mail@markus-jaeckle.de>
2018-06-05 09:22:31 +0200

    added packages

---
+++ Assignment3/WholesalerWebDemo/src/de/htwg_konstanz/ebut/action/Action.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package de.htwg_konstanz.ebut.action;
+
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import de.htwg_konstanz.ebus.wholesaler.demo.IAction;
+
+/**
+ * @author markus
+ *
+ */
+public class Action implements IAction {
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response, ArrayList<String> errorList) {
+		
+		return null;
+	}
+
+	@Override
+	public boolean accepts(String actionName) {
+		
+		return false;
+	}
+
+}
